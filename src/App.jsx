813a781daf3728d6a53b251b412d3ca4f10d93ba
Håkon Åmdal <hakon@aamdal.com>
2019-04-12 13:15:32 +0200

    more work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 const kristoffer = {
   name: "kristoffer"
 };
 const hehe = () => console.log("from the outside");
-const App = () => (
-  <>
-    <h1>Hans-wilhelm</h1>
-    <simple-counter name={JSON.stringify(kristoffer)} count={13} />
-  </>
-);
+const App = () => {
+  const ref = React.createRef();
+  useEffect(() => {
+    ref.current.addEventListener("foo", data => {
+      console.log(data);
+    });
+    ref.current.dispatchEvent(new CustomEvent("init", { detail: { count: 13 } }));
+    console.log(ref);
+  });
+  return (
+    <>
+      <h1>Hello world</h1>
+      <simple-counter ref={ref} name={JSON.stringify(kristoffer)} />
+    </>
+  );
+};
+
 export default App;
--- src/SimpleCounter.js
+++ src/SimpleCounter.js
@@ -1,6 +1,8 @@
 import { html, define } from "hybrids";
 
 export function increaseCount(host) {
+  const e = new Event("foo");
+  host.dispatchEvent(e);
   host.count += 1;
 }
 
@@ -11,13 +13,20 @@ export const SimpleCounter = {
   _name: {
     get: ({ name }) => JSON.parse(name)
   },
-  render: ({ count, _name }) => {
-    return html`
-      <button onclick="${increaseCount}">
-        Count: ${count} Name: ${_name.name}
-      </button>
-    `;
-  }
+  init: {
+    connect: (host, key) => {
+      console.log("hoh");
+      host.addEventListener("init", e => {
+        host["count"] = e.detail.count;
+        host["data"] = e.detil;
+      });
+    }
+  },
+  render: ({ count, _name }) =>
+    html`
+      <h1>hehe</h1>
+      <button onclick=${increaseCount}>${count}</button>
+    `
 };
 
 define("simple-counter", SimpleCounter);
